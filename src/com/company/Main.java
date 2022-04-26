117331a7b56800bc4f349422559f3f1beab5d97b
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-04-26 18:37:37 +0300

    Update to Laba 12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,30 +2,34 @@ package com.company;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void findIP(Matcher matcher)
-    {
+    public static void findIP(Matcher matcher, FileWriter writer) throws IOException {
         if(matcher.find()) {
             System.out.println(matcher.group());
+            writer.write(matcher.group()+"\n");
+            writer.flush();
         }
         else {
             System.out.println("Не найдено");
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        File ip = new File("","ip.txt");
+        if (!ip.exists()){
+            boolean ipIsCreated = ip.createNewFile();
+        }
 
-        String input = "iui 95.152.73.142 енкенек";
+        FileWriter writer = new FileWriter("ip.txt", true);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
         Pattern pattern = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
         Matcher matcher = pattern.matcher(input);
 
-        findIP(matcher);
-
-        String input1 = scanner.nextLine();
-        matcher = pattern.matcher(input1);
-
-        findIP(matcher);
+        findIP(matcher, writer);
     }
 }
